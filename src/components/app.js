f6effbf618566c60859e1f33fd225c4f983db2a7
tungnh91 <tungnh91@gmail.com>
2017-02-25 10:30:51 -0800

    Refactor module .controller to app's own controller

--- src/components/app.js
+++ src/components/app.js
@@ -1,28 +1,21 @@
 angular.module('video-player')
-.controller('videosController', function($scope, $window) {
-  $scope.exampleVideoData = $window.exampleVideoData;
-  $scope.video = $scope.exampleVideoData[0];
-  console.log('hello', $scope);
-  $scope.clickHandler = function() {
-
-  };
-  $scope.selectVideo = function() {
-
-  };
-  console.log('video-player module controller scope: ', $scope);
-})
-
 .directive('app', function() {
   return {
     scope: {
-      exampleVideoData: '<',
       selectVideo: '<'
     },
     controllerAs: 'ctrl',
     bindToController: true,
     controller: function($scope, $window) {
-      
-      console.log('this is the app scope', $scope);
+      this.exampleVideoData = $window.exampleVideoData;
+      this.video = $window.exampleVideoData[0];
+      this.clickHandler = function() {
+
+      };
+      this.selectVideo = function() {
+
+      };
+      console.log('this is the app this', this);
     },
     templateUrl: 'src/templates/app.html'
   };
